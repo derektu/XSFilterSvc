3fe3bf1574e6a338b64cfa5915025f3e487d02a7
derektu <derektu123@gmail.com>
2022-08-01 17:39:08 +0800

    add xsfiltersvc.test (連到XS測試環境)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,19 @@
 module.exports = {
-    apps : [{
-      name   : "xsfiltersvc",
-      script : "./lib/app.js",
-      args : "",
-      watch : true,
-      ignore_watch : [ "node_modules", "logs" ]    
-    }]
+    apps : [
+        {
+            name   : "xsfiltersvc",
+            script : "./lib/app.js",
+            args : "",
+            watch : true,
+            ignore_watch : [ "node_modules", "logs" ]    
+        },
+        {
+            name   : "xsfiltersvc.test",
+            script : "./lib/app.js",
+            args : "--port=8887 --xsdocdb=http://203.67.19.128/xsdocdb.test --xsservice=http://203.67.19.128/xsservicetest",
+            watch : true,
+            ignore_watch : [ "node_modules", "logs" ]    
+        }
+    ]
   }
   
