91601230e950221ffc46ccbb684b56867dff081d
derektu <derektu123@gmail.com>
2023-04-07 19:44:00 +0800

    server update API格式!!

--- test/test_symbolsvc.js
+++ test/test_symbolsvc.js
@@ -3,7 +3,7 @@
 const SymbolSvc = require('../lib/symbolsvc');
 
 describe("Test SymbolSvc", ()=> {
-    it("test Exch 1", async()=> {
+    it.skip("test Exch 1", async()=> {
         let svc = new SymbolSvc();
 
         let symbols = await svc.getSymbolList('1');
--- test/test_xsservice.js
+++ test/test_xsservice.js
@@ -1,24 +1,30 @@
 "use strict";
 
+const moment = require('moment');
+
 const XSService = require('../lib/xsservice');
 
 describe("Test XSService", ()=> {
-    it.skip("test queryFieldData(日)", async()=> {
+    it.skip("queryFieldData(日)", async()=> {
         let svc = new XSService();
 
-        let fields = ['外資買賣超', '投信買賣超'];
+        let fields = ['3054', '3055'];  // 3054=外資買賣超, 3055=投信買賣超
+        let startDate = '20230101';
+        let endDate = moment().format("YYYYMMDD");
 
-        let data = await svc.queryFieldData('2330.TW', '8', fields, 20);
+        let data = await svc.queryFieldData('2330.TW', '8', fields, startDate, endDate);
 
         console.dir(data, {depth: null});
     })
 
-    it.skip("test queryFieldData(日 with 日期)", async()=> {
+    it.skip("queryFieldData(日)_with_日期", async()=> {
         let svc = new XSService();
 
-        let fields = ['日期', '3', '收盤價', '外資買賣超'];
+        let fields = ['3', '8', '3054'];    //3=日期, 8=收盤價, 3054=外資買賣超
 
-        let data = await svc.queryFieldData('2330.TW', '8', fields, 20);
+        let startDate = '20230101';
+        let endDate = moment().format("YYYYMMDD");
+        let data = await svc.queryFieldData('2330.TW', '8', fields, startDate, endDate);
 
         console.dir(data, {depth: null});
     })
@@ -26,9 +32,11 @@ describe("Test XSService", ()=> {
     it.skip("test queryFieldData(月)", async()=> {
         let svc = new XSService();
 
-        let fields = ['月營收', '3093'];
+        let fields = ['3091', '3093'];  //3091=月營收, 3093=月營收年增率
 
-        let data = await svc.queryFieldData('2330.TW', '10', fields, 20);
+        let startDate = '20230101';
+        let endDate = moment().format("YYYYMMDD");
+        let data = await svc.queryFieldData('2330.TW', '10', fields, startDate, endDate);
 
         console.dir(data, {depth: null});
     })
